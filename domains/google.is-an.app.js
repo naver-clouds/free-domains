d72eaf2e6a78fe4785e9149dd883a4bfb927ed30
enkiark <96149033+enkiark@users.noreply.github.com>
2023-05-23 05:36:13 +0900

    Create google.is-an.app.js

---
+++ domains/google.is-an.app.js
@@ -0,0 +1,12 @@
+addSubDomain({
+  description: 'A website for my google bot',
+  domain: 'is-an.app',
+  subdomain: 'aviana',
+  owner: {
+    email: 'contact@naver.com',
+  },
+  record: {
+    CNAME: 'cname.vercel-dns.com'
+  },
+  proxy: true
+});
